f84028196adcabd7fb985f9d8934f486db9b2148
Sergei Popinevskii <gurza000@gmail.com>
2022-12-29 11:02:57 +0300

    reorganize project structure

--- common.go
+++
@@ -1,6 +0,0 @@
-package dc
-
-// TableName is the fully qualified name of a table,
-// where the last item in the array is the table name
-// and any earlier items represent the namespacing of the table name.
-type TableName []string
--- dc.go
+++ dc.go
@@ -8,3 +8,8 @@ type Agent struct {
 type Config struct {
 	DB string
 }
+
+// TableName is the fully qualified name of a table,
+// where the last item in the array is the table name
+// and any earlier items represent the namespacing of the table name.
+type TableName []string
