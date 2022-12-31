97b3d12f34da4e03552119494d9cb9097dfc9ffc
Sergei Popinevskii <gurza000@gmail.com>
2022-12-31 15:22:49 +0300

    add json attributes for marshaling

--- dc.go
+++ dc.go
@@ -9,18 +9,18 @@ type Agent struct {
 type Config struct {
 	// DB is the database name,
 	// e.g. "/ru-central1/HwYLaqGaBt8hmM85wQq2/eCGNADsfxsbFyNrWPZCz".
-	DB string
+	DB string `json:"db"`
 
 	// Tables is a list of tables to be exposed.
-	Tables []string
+	Tables []string `json:"tables,omitempty"`
 
 	// InludeMetaTable is whether to expose meta tables.
-	InludeMetaTable bool
+	InludeMetaTable bool `json:"include_sqlite_meta_tables,omitempty"`
 
-	// InludeMainSchema Flag is whether to opt into exposing tables
-	// by their fully qualified names, e.g. ["main", "table42"],
-	// instead of just ["table42"].
-	ExplicitMainSchema bool
+	// InludeMainSchema is whether to opt into exposing tables
+	// by their fully qualified names,
+	// e.g. ["main", "table42"] instead of just ["table42"].
+	ExplicitMainSchema bool `json:"explicit_main_schema,omitempty"`
 }
 
 // TableName is the fully qualified name of a table,
