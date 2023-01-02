5e94ae27b78c403bdf155d8f1e0fed8269192a9f
Sergei Popinevskii <gurza000@gmail.com>
2023-01-02 19:24:00 +0300

    start to describe Capabilities struct

--- capabilities.go
+++ capabilities.go
@@ -7,21 +7,36 @@ type CapabilitiesService interface {
 }
 
 type CapabilitiesResponse struct {
-	Capabilities Capabilities `json:"capabilities"`
+	DisplayName string `json:"display_name,omitempty"`
+
+	ReleaseName string `json:"release_name,omitempty"`
 
 	// ConfigSchemas represents an agent configuration.
 	ConfigSchemas ConfigSchemasResponse `json:"config_schemas"`
 
-	DisplayName string `json:"display_name,omitempty"`
-
-	ReleaseName string `json:"release_name,omitempty"`
+	Capabilities Capabilities `json:"capabilities"`
 }
 
 type Capabilities struct {
-	ConfigSchema OpenApiSchema `json:"config_schema"`
-
-	OtherSchemas map[string]OpenApiSchema `json:"other_schemas"`
+	DataSchema DataSchemaCapabilities `json:"data_schema,omitempty"`
 }
+
+type DataSchemaCapabilities struct {
+	ColumnNullability ColumnNullability `json:"column_nullability,omitempty"`
+
+	// SupportsForeignKeys is whether tables can have foreign keys.
+	SupportsForeignKeys bool `json:"supports_foreign_keys,omitempty"`
+
+	// SupportsPrimaryKeys is whether tables can have primary keys.
+	SupportsPrimaryKeys bool `json:"supports_primary_keys,omitempty"`
+}
+
+type ColumnNullability string
+
+const (
+	ColumnNullabilityNull           ColumnNullability = "only_nullable"
+	ColumnNullabilityNullAndNotNull ColumnNullability = "nullable_and_non_nullable"
+)
 
 // ConfigSchemasResponse ...
 //
@@ -40,7 +55,6 @@ type OpenApiSchema struct {
 	Type OASDataType `json:"type,omitempty"`
 }
 
-// OASDataType is a set of OpenAPI schema data types.
 type OASDataType string
 
 const (
