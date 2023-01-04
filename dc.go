98bc6fe6f657c3d2e6ef7c0607fde07bb97d86c8
Sergei Popinevskii <gurza000@gmail.com>
2023-01-04 20:51:30 +0300

    delete Config since it is implementation specific

--- dc.go
+++ dc.go
@@ -5,24 +5,6 @@ type Agent struct {
 	Schema SchemaService
 }
 
-// Config represents the configuration properties of the DataConnector Agent.
-type Config struct {
-	// DB is the database name,
-	// e.g. "/ru-central1/HwYLaqGaBt8hmM85wQq2/eCGNADsfxsbFyNrWPZCz".
-	DB string `json:"db"`
-
-	// Tables is a list of tables to be exposed.
-	Tables []string `json:"tables,omitempty"`
-
-	// InludeMetaTable is whether to expose meta tables.
-	InludeMetaTable bool `json:"include_sqlite_meta_tables,omitempty"`
-
-	// InludeMainSchema is whether to opt into exposing tables
-	// by their fully qualified names,
-	// e.g. ["main", "table42"] instead of just ["table42"].
-	ExplicitMainSchema bool `json:"explicit_main_schema,omitempty"`
-}
-
 // TableName is the fully qualified name of a table,
 // where the last item in the array is the table name
 // and any earlier items represent the namespacing of the table name.
