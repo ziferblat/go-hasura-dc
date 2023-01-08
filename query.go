49232574c2e943236c3d942de98c33ecacc5cd6f
Sergei Popinevskii <gurza000@gmail.com>
2023-01-08 22:07:44 +0300

    define query request

--- query.go
+++ query.go
@@ -3,7 +3,17 @@ package dc
 import "context"
 
 type QueryService interface {
-	Exec(ctx context.Context, conf any, q any) (*ExecQueryResponse, error)
+	Exec(ctx context.Context, conf any, q Query) (*ExecQueryResponse, error)
+}
+
+type QueryRequest struct {
+	Table              TableName            `json:"table"`
+	Query              Query                `json:"query"`
+	TableRelationships []TableRelationships `json:"table_relationships"`
 }
 
 type ExecQueryResponse struct{}
+
+type Query struct{}
+
+type TableRelationships struct{}
