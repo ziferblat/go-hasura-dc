96023d209571b26982c564457ec7f975961f9d51
Sergei Popinevskii <gurza000@gmail.com>
2023-01-11 17:32:19 +0300

    try to decode val into scalar type

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -2,9 +2,11 @@ package scanner
 
 import (
 	"database/sql"
+	"strconv"
 )
 
 // Scan copies the columns in the current row into the map pointed at by dest.
+// If possible, it converts val to scalar type.
 func ScanMap(rs *sql.Rows, dest map[string]interface{}) error {
 	return copy(rs, dest)
 }
@@ -25,8 +27,28 @@ func copy(rs *sql.Rows, dest map[string]interface{}) error {
 	}
 	for i, col := range cols {
 		val := ptrs[i].(*interface{})
-		dest[col] = *val
+		dest[col] = decode(*val)
 	}
 
 	return nil
+}
+
+func decode(val interface{}) interface{} {
+	bb, ok := val.([]byte)
+	if !ok {
+		return val
+	}
+	s := string(bb)
+
+	if rval, err := strconv.Atoi(s); err == nil {
+		return rval
+	}
+	if rval, err := strconv.ParseFloat(s, 64); err == nil {
+		return rval
+	}
+	if rval, err := strconv.ParseBool(s); err == nil {
+		return rval
+	}
+
+	return s
 }
